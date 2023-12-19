53136093112134f14a1a9c1adb30dbe946365b31
TodorKV <todorv1@gmail.com>
2023-12-19 23:31:44 +0200

    reg form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,96 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { FormBuilder, FormsModule, Validators } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ToastModule,
+    RouterOutlet,
+    CardModule,
+    FormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+  ],
+  providers: [MessageService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-app';
+
+  registerForm = this.fb.group(
+    {
+      fullName: [
+        '',
+        [Validators.required, Validators.pattern(/^[a-zA-Z]+(?: [a-zA-Z]+)*$/)],
+      ],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    },
+    {
+      validators: passwordMatchValidator,
+    }
+  );
+
+  constructor(
+    private fb: FormBuilder,
+    private messageService: MessageService,
+    private router: Router
+  ) {}
+
+  get fullName() {
+    return this.registerForm.controls['fullName'];
+  }
+
+  get email() {
+    return this.registerForm.controls['email'];
+  }
+
+  get password() {
+    return this.registerForm.controls['password'];
+  }
+
+  get confirmPassword() {
+    return this.registerForm.controls['confirmPassword'];
+  }
+
+  submitDetails() {
+    const postData = { ...this.registerForm.value };
+    delete postData.confirmPassword;
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Register successfully',
+    });
+  }
 }
+
+export const passwordMatchValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  if (!password || !confirmPassword) {
+    return null;
+  }
+
+  return password.value === confirmPassword.value
+    ? null
+    : { passwordMismatch: true };
+};
